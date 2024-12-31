b784d05bb50140e4990857328da032bf20e0ce00
Kowebtech Ict <info@koweb.com.ng>
2024-12-31 04:03:20 +0100

    Update

--- PLIMain/views/Dashboard.vue
+++ PLIMain/views/Dashboard.vue
@@ -1,8 +1,10 @@
 <template>
     <div>
+        <user-card></user-card>
     </div>
 </template>
 <script setup>
+    import UserCard from '@module/propertyListing/components/UserCard.vue'
 </script>
 <style lang="scss" scoped>
 </style>
---
+++ components/UserCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="testimonial-block"
+    data-slick-index="2"
+    aria-hidden="false"
+    tabindex="0"
+  >
+    <img src="/Public/Themes/jonikinsTheme/Assets/media/testimonial/img-3.png" class="w-100" alt="" />
+    <div class="data">
+        <div class="user-img">
+        <img src="/Public/Themes/jonikinsTheme/Assets/media/user/img-3.png" alt="" />
+        </div>
+        <h6 class="name">Ava Thompson</h6>
+        <p>
+        "The real estate agents were incredibly responsive and helpful throughout
+        the entire process. I couldn't have asked for a better experience."
+        </p>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.testimonial-block {
+    width: 100%;
+    max-width: 348px;
+}
+.data {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    top: -50px;
+}
+</style>
